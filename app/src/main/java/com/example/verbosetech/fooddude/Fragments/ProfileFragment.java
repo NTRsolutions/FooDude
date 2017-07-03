e82ffc1548c480b7f1353d57c01afdab45c73fdb
sagarsharma25 <sagarsharma708@gmail.com>
2017-07-03 22:19:47 +0530

    Ninth Commit

--- app/src/main/java/com/example/verbosetech/fooddude/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/verbosetech/fooddude/Fragments/ProfileFragment.java
@@ -9,8 +9,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.verbosetech.fooddude.Activities.TestActivity;
 import com.example.verbosetech.fooddude.Models.PastOrders;
 import com.example.verbosetech.fooddude.Models.Profile;
 import com.example.verbosetech.fooddude.Others.PastOrdersAdapter;
@@ -24,7 +28,7 @@ import java.util.List;
  * Created by sagar on 28/6/17.
  */
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener {
 
     private View view;
     private RecyclerView.LayoutManager layoutManager;
@@ -32,10 +36,26 @@ public class ProfileFragment extends Fragment {
     RecyclerView recyclerView;
     List<Profile> profileList;
 
+    ImageView edit;
+    TextView add,save;
+    EditText name,mail,phone;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.profile_layout,container,false);
+
+        edit=(ImageView) view.findViewById(R.id.edit);
+        add=(TextView)view.findViewById(R.id.add_new);
+        name=(EditText)view.findViewById(R.id.name);
+        mail=(EditText)view.findViewById(R.id.gmail);
+        phone=(EditText)view.findViewById(R.id.phone_num);
+        save=(TextView)view.findViewById(R.id.save);
+
+
+        edit.setOnClickListener(this);
+        add.setOnClickListener(this);
+        save.setOnClickListener(this);
 
         recyclerView = (RecyclerView) view.findViewById(R.id.profile_grid);
         recyclerView.setHasFixedSize(true);
@@ -63,5 +83,28 @@ public class ProfileFragment extends Fragment {
         });
 
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+        switch (v.getId()){
+
+            case R.id.edit:
+                name.setEnabled(true);
+                mail.setEnabled(true);
+                phone.setEnabled(true);
+                save.setVisibility(View.VISIBLE);
+                edit.setVisibility(View.GONE);
+                break;
+            case R.id.save:name.setEnabled(false);
+                mail.setEnabled(false);
+                phone.setEnabled(false);
+                save.setVisibility(View.GONE);
+                edit.setVisibility(View.VISIBLE);
+                break;
+            case R.id.add_new:break;
+        }
     }
 }
